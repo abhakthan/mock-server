14d44a8e3ea5fff794e932d682d3b6e0f9cdcc0c
Ajay Bhakthan <abhakthan@it-abhakthanm1.corporate.intersections.com>
2016-06-17 17:12:31 -0400

    single file expectations

--- server.js
+++
@@ -1,9 +0,0 @@
-var mockServer = require('mockserver-grunt');
-
-mockServer.start_mockserver({
-                serverPort: 1080,
-                proxyPort: 1090,
-                verbose: false
-            });
-
-//mockserver.stop_mockserver();
